ea73da6a8b01874225063fbb3ce0017ec232b718
Bhupalan K <bhupalan1711@gmail.com>
2024-07-30 19:51:15 -0700

    uploaded

--- src/components/bodyPart/BodyPart.js
+++ src/components/bodyPart/BodyPart.js
@@ -4,10 +4,14 @@ import './BodyPart.css'
 const BodyPart = ({ item, bodyPart, setBodyPart }) => {
   return (
     <div className='bodypart' onClick={() => setBodyPart(item)} >
-      <a style={{
-        borderTop: bodyPart === item ? '2px solid white' : '',
-        paddingTop: '4px'
-      }} ><i class="fa-solid fa-dumbbell"></i></a>
+      <button 
+        style={{
+          borderTop: bodyPart === item ? '2px solid white' : '',
+          paddingTop: '4px',
+        }}
+      >
+        <i className="fa-solid fa-dumbbell"></i>
+      </button>
       <p>{item}</p>
     </div>
   )
